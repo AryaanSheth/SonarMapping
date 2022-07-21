4c1685e3cfe0844e762d6da15e15bba99e9e6ed9
Aryaan Sheth <avhsheth03@gmail.com>
2022-07-21 18:04:08 -0400

    updated panel get added object matrix

--- src/Gui/gui.java
+++ src/Gui/gui.java
@@ -2,37 +2,58 @@ package Gui;
 
 import LogicPkg.Logic;
 import java.awt.Color;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class gui{
 
-    public static void screenSettings(JFrame frame, JPanel panel, int dimension){
-        frame.setSize(dimension, dimension);
+    public static void screenSettings(JFrame frame, int dimension){
+        frame.setSize(dimension+13, dimension+37);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // set default close operation
         frame.setVisible(true); // make the fram visible
         frame.setTitle("Matrix");
-        frame.setLayout(null);
+        //frame.setLayout(null);
         frame.setResizable(false);
     }
 
-    public static void generatePanels(float[][] matrix, JFrame frame, JPanel panel, int dimension){
-        panel.setBackground(Color.CYAN);
-        panel.setBounds(0, 0, dimension/matrix.length, dimension/matrix.length);
-        panel.add(panel);
+    public static void generatePanels(float[][] matrix, JFrame frame, JPanel[][] panel, int dimension){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                panel[i][j] = new JPanel();
+                panel[i][j].setBounds(j*(dimension/matrix[i].length), i*(dimension/matrix.length), dimension/matrix[i].length, dimension/matrix.length);
+                panel[i][j].setBackground(new Color((int)(matrix[i][j]*100)+50, (int)(matrix[i][j]*50)+30, (int)(matrix[i][j]*90)+30));
+                frame.add(panel[i][j]);
+            }
+        }
+    }
+
+    public static JPanel[][] objectMatrix(float[][] matrix){
+        JPanel panels[][] = new JPanel[matrix.length][matrix[0].length]; // create a 2D array of JPanels
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                JPanel panel = new JPanel(); // creates a new JPanel obj for each element in the matrix
+                panels[i][j] = panel; // sets current JPanel to current index of matrix
+            }
+        }
+        return panels;
     }
 
     public static void main(String[] args){
         Logic logic = new Logic();  // instantiate logic object from the logic package
         JFrame frame = new JFrame(); // instantiate frame object from the javax.swing package
-        JPanel panel = new JPanel(); // instantiate panel object from the javax.swing package
 
-        final int dimension = 500;
+        int dimension = 500; // set dimension of frame
 
-        float[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
-        logic.printMatirx(matrix);
+        float[][] matrix = {{1,2,3},{4,5,6},{7,8,9}}; // test matrix
 
-        screenSettings(frame, panel, dimension); // call screenSettings method to set up the screen
-        generatePanels(matrix, frame, panel, dimension); // call generatePanels method to generate the panels
+        float[][] normMatrix = logic.normalizeMatrix(matrix, logic.findMaxMatrix(matrix));
+        //logic.printMatirx(matrix);
+        //System.out.println(objectMatrix(matrix));
+        objectMatrix(matrix);
+
+        screenSettings(frame, dimension); // call screenSettings method to set up the screen
+        generatePanels(normMatrix, frame, objectMatrix(matrix), dimension); // call generatePanels method to generate the panels
     }
 }
