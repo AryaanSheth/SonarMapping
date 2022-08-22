b14a4eb495b2be04be3bae0cbc78934e22ad7b7d
Aryaan Sheth <avhsheth03@gmail.com>
2022-08-22 11:02:21 -0400

    refactor to component model

--- src/Gui/display/display.java
+++ src/Gui/display/display.java
@@ -6,19 +6,6 @@ import javax.swing.JPanel;
 import javax.swing.JFrame;
 
 public class display {
-    /**
-     * 
-     * @param frame
-     *  
-     * Defines frame attributes for main display.
-     */
-    public static void screenSettings(JFrame frame, int dimension) {
-        frame.setSize(dimension+13, dimension+37);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        frame.setTitle("Matrix Display");
-        frame.setResizable(false);
-    }
 
 
     /**
@@ -27,7 +14,7 @@ public class display {
      * 
      * Creates an instance of a JPanel object for each entry in the matrix that defines the window.
      */
-    public static JPanel[][] objectMatrix(float[][] matrix){
+    public JPanel[][] objectMatrix(float[][] matrix){
         JPanel panels[][] = new JPanel[matrix.length][matrix[0].length]; // initilizes a 2D array of each element as a JPanel obj
 
         for(int i = 0; i < matrix.length; i++){
@@ -41,37 +28,43 @@ public class display {
     }
 
 
-    public static void generatePanels(float[][] matrix, JFrame frame, JPanel[][] panel, int dimension){
+    public void generatePanels(float[][] matrix, JFrame frame, JPanel[][] panel, int dimension){
         for(int i = 0; i < matrix.length; i++){
             for(int j = 0; j < matrix.length; j++){
                 panel[i][j] = new JPanel();
+
                 // defines the size and position of the JPanel object based on the dimensions of the input matrix and the dimensions of the window
-                panel[i][j].setBounds(j*(dimension/matrix.length), i*(dimension/matrix.length), dimension / matrix[i].length, dimension / matrix.length);
-                panel[i][j].setBackground(new Color((int) (matrix[i][j] * 150), (int) (matrix[i][j] * 50), (int) (matrix[i][j] * 90)));
+                panel[i][j].setBounds(j*(dimension/matrix.length), i*(dimension/matrix.length), 
+                dimension / matrix[i].length, dimension / matrix.length);
+
+                // defines the color of the JPanel objects in the matrix
+                panel[i][j].setBackground(new Color((int) (matrix[i][j] * 150), 
+                (int) (matrix[i][j] * 50), (int) (matrix[i][j] * 90)));
+
                 frame.add(panel[i][j]);
             }
         }
     }
 
 
-    public static void main(String[] args) {
-        logic logic = new logic(); // instantiate logic object from the logic package
-        JFrame frame = new JFrame(); // instantiate frame object from the javax.swing packag
+    // public static void main(String[] args) {
+    //     logic logic = new logic(); // instantiate logic object from the logic package
+    //     JFrame frame = new JFrame(); // instantiate frame object from the javax.swing packag
 
-        int dimension = 500;
+    //     int dimension = 500;
 
-        float[][] matrix = { {1, 2, 3, 4}, { 5, 6, 7, 8}, { 9, 10, 11, 12 }, {13, 14, 15, 16}}; // test matrix
-        logic.printMatrix(matrix);
+    //     float[][] matrix = { {1, 2, 3, 4}, { 5, 6, 7, 8}, { 9, 10, 11, 12 }, {13, 14, 15, 16}}; // test matrix
+    //     logic.printMatrix(matrix);
 
-        float[][] normMatrix = logic.normalizeMatrix(matrix, logic.findMaxMatrix(matrix));
-        // logic.printMatirx(matrix);
-        System.out.println(objectMatrix(matrix));
-        objectMatrix(matrix);
+    //     float[][] normMatrix = logic.normalizeMatrix(matrix, logic.findMaxMatrix(matrix));
+    //     // logic.printMatirx(matrix);
+    //     System.out.println(objectMatrix(matrix));
+    //     objectMatrix(matrix);
 
-        screenSettings(frame, dimension); // call screenSettings method to set up the screen
-        generatePanels(normMatrix, frame, objectMatrix(matrix), dimension); // call generatePanels method to generate
-                                                                            // the panels
-    }
+    //     screenSettings(frame, dimension); // call screenSettings method to set up the screen
+    //     generatePanels(normMatrix, frame, objectMatrix(matrix), dimension); // call generatePanels method to generate
+    //                                                                         // the panels
+    // }
 
 }
 
--- src/Gui/settings/settings.java
+++ src/Gui/settings/settings.java
@@ -2,45 +2,64 @@ package gui.settings;
 
 import logic.logic;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JPanel;
 import javax.swing.JFrame;
 import javax.swing.JButton;
 
 // creates a second window that allows end users to edit the settings of the gui
 
-public class settings {
-    /**
-     * 
-     * @param frame
-     *  
-     * Defines frame attributes for main display.
-     */
-    public static void screenSettings(JFrame frame, int dimension) {
-        frame.setSize(dimension+13, dimension+37);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        frame.setTitle("Matrix Display");
-        frame.setResizable(false);
-    }
+public class settings implements ActionListener {
+
+    public boolean state = false;
 
     // creates a button object on the frame
-    public static JButton button(JFrame frame, String text){
+    public boolean button(JFrame frame, String text) {
         JButton button = new JButton(text);
+
         button.setBounds(50, 50, 150, 50);
         frame.add(button);
-        return button;
+
+        button.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (state == false) {
+                    state = true;
+                } else {
+                    state = false;
+                }
+                System.out.println(state);
+            }
+        });
+
+        // button.addActionListener(e -> {
+        // if(state == false){
+        // button.setText("ON");
+        // state = true;
+        // }else{
+        // button.setText("OFF");
+        // state = false;
+        // }
+        // });
+        return state;
     }
-    
 
-
-
-    public static void main(String args[]){
-        int dimension = 500;
-        JFrame frame = new JFrame();
-
-        screenSettings(frame, dimension);
-        button(frame, "Show Elevation");
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("states was changed");
 
     }
+
+    // public static void main(String args[]) {
+    //     int dimension = 500;
+    //     JFrame frame = new JFrame();
+    //     System.out.println(state);
+
+    //     screenSettings(frame, dimension);
+    //     button(frame, "Show Elevation");
+
+    // }
 
 }
